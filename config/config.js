10b311d2d78adec4cf43851180fd700b265ad054
Fabrizio D'Isidoro <fabrizioandres98@gmail.com>
2026-06-02 00:08:52 -0300

    Test

--- config/config.js
+++ config/config.js
@@ -7,21 +7,19 @@ module.exports = {
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        dialect: 'mysql',
-        dialectOptions: {
-            ssl: false
-        }
+        dialect: 'mysql'
     },
     production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        username: process.env.MYSQLUSER,
+        password: process.env.MYSQLPASSWORD,
+        database: process.env.MYSQLDATABASE,
+        host: process.env.MYSQLHOST,
+        port: process.env.MYSQLPORT,
         dialect: 'mysql',
         dialectOptions: {
             ssl: {
-                require: false,
+                require: true,
+                rejectUnauthorized: false
             }
         }
     }
