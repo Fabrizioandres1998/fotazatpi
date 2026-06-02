1685428a06e3445e528b27d12aca10e42cebfd58
Fabrizio D'Isidoro <fabrizioandres98@gmail.com>
2026-06-01 21:59:17 -0300

    fix error session

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const session = require('express-session');
 const { Usuario } = require('./models');
 
 //IMPORTACION RUTAS
